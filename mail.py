713999f10f7b7502b1d67524b4f468f228025c0a
jmpark6846 <jmpark6846@github.com>
2018-10-29 11:12:44 +0900

    mail 함수 작성

---
+++ mail.py
@@ -0,0 +1,58 @@
+import re
+import dns.resolver
+import socket
+import smtplib
+
+def validate_email(mail):
+
+    if not check_mail_syntax(mail):
+        raise ValueError('bad syntax')
+
+    domain_name = mail.split('@')[1]
+
+    mx_record = get_mx_record(domain_name)
+
+    host = socket.gethostname()
+
+    server = smtplib.SMTP()
+    server.set_debuglevel(0)
+
+    server.connect(mx_record)
+    server.helo(host)
+    server.mail('hi@hi.com')
+
+
+    code, message = server.rcpt(str(mail))
+
+    if code == 250:
+        print('valid')
+        # return True
+    else:
+        print('invalid')
+        # return False
+
+    server.quit()
+
+
+def check_mail_syntax(mail):
+    match = re.match('^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$', mail)
+    return False if match == None else True
+
+
+def get_mx_record(domain_name):
+    try:
+        records = dns.resolver.query(domain_name, 'MX')
+    except Exception:
+        print('invalid')
+        raise ValueError('bad domain name')
+
+    mx_record = records[0].exchange
+    mx_record = str(mx_record)
+    return mx_record
+
+def check_mail_accounts():
+    pass
+
+
+solution('jmpark6846@gmail.com')
+
