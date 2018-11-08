73a0287f8a1862395190fba7e81fe655949531fc
jmpark6846 <jmpark6846@github.com>
2018-11-08 23:46:58 +0900

    user login, logout, signup을 업데이트

--- checkmail/views.py
+++ checkmail/views.py
@@ -1,8 +1,12 @@
+from django.shortcuts import render
 from rest_framework.decorators import api_view, throttle_classes
 from rest_framework.throttling import UserRateThrottle, AnonRateThrottle
 from rest_framework.response import Response
 from .email import validate_email
 
+
+def index(request):
+    return render(request, 'checkmail/index.html')
 
 @api_view(['GET'])
 @throttle_classes([AnonRateThrottle, UserRateThrottle])
--- proj/settings.py
+++ proj/settings.py
@@ -138,6 +138,10 @@ REST_FRAMEWORK = {
     }
 }
 
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = LOGIN_URL
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
